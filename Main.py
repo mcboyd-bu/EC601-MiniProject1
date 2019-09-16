16697a066d271ca2e8a2be250d903cd7a637f3fe
Matthew Boyd <41444594+mcboyd-bu@users.noreply.github.com>
2019-09-16 18:06:11 -0400

    Tweak steps in Main notes

--- Main.py
+++ Main.py
@@ -13,18 +13,20 @@ print(g)
 
 
 # Outline of steps:
+# 0. Ask for user type (Netflix or watcher)
 # 1. Ask user for # or @ to look-up
 # 2. Call Twitter API to get back list of tweets in timeframes
 #   - How do we know date of release for show to set timeframes? Ask for that as well?
-#   - Return tweet text results stripped out of JSON API result as array of strings (or save to local file?) 
-# 3. Call function to (randomly) extract a subset of the tweets for analysis (certain number per day, maybe?)
-#	- Return as array/text file
+#   - Return tweet text results stripped out of JSON API result as array of strings (or save to local file?)
+# NO MORE 3. Call function to (randomly) extract a subset of the tweets for analysis (certain number per day, maybe?)
+# NO MORE	- Return as array/text file
 # 4. Call Google API to analyze each tweet in subset
 #	- Return sentiment and magnitude
 #	- Store returned values in array/object
 # 5. Call function to convert sentiments and magnitudes into overall daily sentiment values
-# 6. Call library to plot chart of daily values
-#	- Plotly: https://plot.ly/python/getting-started/
+# 6. Display results
+# NO MORE 6. Call library to plot chart of daily values
+# NO MORE	- Plotly: https://plot.ly/python/getting-started/
 #
 # ! Probably use ProgressBar2 to display progress for user as the many above tasks are happening...
 # ! https://pypi.org/project/progressbar2/
